545e36e6806748ca811be5785c069a581933894c
lohanny <126508122+lohanny253@users.noreply.github.com>
2024-10-02 17:03:16 -0400

    Add files via upload

---
+++ lp/at1.js
@@ -0,0 +1,11 @@
+let frutas =["Uva", "Maça", "Manga"];
+console.log(frutas);
+frutas.sort();
+
+//adciona a fruta Maracujá ao final da lista
+frutas.push("Maracujá")
+console.log(frutas);
+
+//Remove o primeiro nome da lista
+frutas.shift()
+console.log(frutas);
